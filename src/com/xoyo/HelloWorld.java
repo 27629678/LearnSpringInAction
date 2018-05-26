39e1d8d360387550d1113530bd947e4ad6db6c01
yuxiaohua <hzyuxiaohua@gmail.com>
2018-05-26 22:04:11 +0800

    remove useless import packages

--- src/com/xoyo/HelloWorld.java
+++ src/com/xoyo/HelloWorld.java
@@ -1,24 +1,7 @@
 package com.xoyo;
 
-import com.xoyo.knights.BraveKnight;
 import com.xoyo.knights.Knight;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.MessageSourceResolvable;
-import org.springframework.context.NoSuchMessageException;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.ResolvableType;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.Resource;
-
-import java.io.IOException;
-import java.lang.annotation.Annotation;
-import java.util.Locale;
-import java.util.Map;
 
 public class HelloWorld {
 
