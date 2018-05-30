ba0c1c0eac65f3e9367de041e20caa000d2b4d2d
yuxiaohua <hzyuxiaohua@gmail.com>
2018-05-30 21:36:35 +0800

    add compact disc demo

--- src/com/xoyo/HelloWorld.java
+++ src/com/xoyo/HelloWorld.java
@@ -1,9 +1,17 @@
 package com.xoyo;
 
 import com.xoyo.knights.Knight;
+import com.xoyo.soundsystem.CDPlayerConfig;
+import com.xoyo.soundsystem.CompactDisc;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 
+@ContextConfiguration(classes = CDPlayerConfig.class)
 public class HelloWorld {
+
+    @Autowired
+    private CompactDisc cd;
 
     public static void main(String[] args) {
         System.out.println("hello, world.");
@@ -18,6 +26,9 @@ public class HelloWorld {
         catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
+        HelloWorld instance = new HelloWorld();
+        instance.cd.play();
 
     }
 
---
+++ src/com/xoyo/soundsystem/CompactDisc.java
@@ -0,0 +1,5 @@
+package com.xoyo.soundsystem;
+
+public interface CompactDisc {
+    void play();
+}
